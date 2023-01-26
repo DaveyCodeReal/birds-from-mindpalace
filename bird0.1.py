eb53ad7baec73b47653dad02a1215361d5aae542
DaveyCodeReal <123515157+DaveyCodeReal@users.noreply.github.com>
2023-01-26 10:57:59 -0800

    Add files via upload

---
+++ bird0.1.py
@@ -0,0 +1,28 @@
+
+import random
+
+birdName1  = ['Great', 'Hover', 'Shrieking', 'Common', 'Rare']
+print(random.choice(birdName1), end=' ')
+
+birdName2 = ['Gull', 'Flaplet', 'Nightjar', 'Sparrow', 'Finch']
+print(random.choice(birdName2))
+
+wingDesc = ['Olive green', 'Sparkly pink and gold', 'Stunningly silver', 'Nearly transparent blue', 'A great many little'] 
+print(random.choice(wingDesc), end='')
+
+print (' wings and', end=' ')
+
+addDesc = ['bold red spots on breast.', 'sad little face.', 'long gangly legs.', 'long thick beak.', 'fluffy little muttonchops.', 'tiny pathetic beak.', 'long brown tail.'] 
+print(random.choice(addDesc))
+
+habPreface = ['Usually found in', 'Generally found in', 'Typically found in', 'Most often found in', 'Can be found in', 'Look for it in'] 
+print(random.choice(habPreface), end=' ')
+
+birdHabitat = ['Peru', 'Northern Ireland', 'Fresno', 'Cold Stone Creamery', 'Oakland CA', 'Large hollow trees with cookie-making elves.', 'The perfume section.', 'Grocery Outlet, bargain market', 'Brooklyn, New York delicatesens', 'Boulder, Colorado', 'The greater Sacramento area'] 
+print(random.choice(birdHabitat))
+
+callPreface = ['The call is', 'The call is', 'The most common call is', 'The typical call is', 'Vocalization is', 'Listen for its call,', 'Often heard before seen. Listen for', 'Typical song is'] 
+print(random.choice(callPreface), end=' ')
+
+birdCall = ['X', 'X', 'X', 'X', 'X', 'X'] 
+print(random.choice(birdCall))
